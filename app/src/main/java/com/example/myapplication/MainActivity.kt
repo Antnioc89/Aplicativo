9ac59ae801628364cc4d431f63ad7c5a3a33a27f
Antonio <antnioc89@gmail.com>
2023-12-06 17:51:04 -0300

    0.1.0

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,13 +9,14 @@ import android.os.Looper
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
         supportActionBar?.hide()
 
         Handler(Looper.getMainLooper()).postDelayed({
             val splash = Intent(this, MainActivity2::class.java)
+            val splash2 = Intent(this, MainActivity3::class.java)
             startActivity(splash)
-        },4000)
+        }, 4000)
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity3 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+
+        val botao = findViewById<Button>(R.id.button3)
+
+        botao.setOnClickListener{
+
+            irParaQuartaTela()
+
+        }
+
+
+    }
+
+    private fun irParaQuartaTela() {
+        val botao3 = Intent(this, MainActivity4::class.java)
+        startActivity(botao3)
     }
 }
